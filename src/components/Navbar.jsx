1f6e7bd1dd04c957d96b2929d4b3aac2b8f5c688
sdiakhate <liousadiakhate20@gmail.com>
2023-11-06 11:12:55 +0100

    fix logo link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import "../assets/styles/navBar.css";
+import logo from "../assets/images/logo.jpg";
 
 function Navbar() {
   return (
     <header className="d-flex align-items-center p-5">
-      <img src="src/assets/images/logo.jpg" alt="logo" />
+      <img src={logo} alt="logo" />
       <p>Atlas Home</p>
     </header>
   );
